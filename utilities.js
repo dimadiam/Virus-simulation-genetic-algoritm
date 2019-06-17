594e2eb8ff1ee7c21b42b333f07149714275b4d7
dimadiam <43180314+dimadiam@users.noreply.github.com>
2019-06-17 15:20:04 +0300

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -3,33 +3,22 @@ function rnd(min, max) {
     rand = Math.round(rand);
     return rand === -0 ? 0 : rand;
 }
-function formatNumber(n) {
-    n = n + '';
-    var res = [], i = n.length - 3, v = n.length % 3;
 
-    if(v === 0) {
-        while(1) {
-            if(i < -2) break;
-            res.push( n.substr(i, 3) );
-            i -= 3;
-        }
-    } else if(v === 1) {
-        i = n.length - 3;
-        while(1) {
-            if(i < -1) break;
-            res.push( n.substr(i, 3) );
-            i -= 3;
-        }
-        res.push(n[0]);
-    } else if(v === 2) {
-        i = n.length - 3;
-        while(1) {
-            if(i < 0) break;
-            res.push( n.substr(i, 3) );
-            i -= 3;
-        }
-        res.push(n.substr(0, 2));
-    }
-    res = res.reverse();
-    return res.join(' ');
+function formatNumber(n) {
+    var res = (n + '').split(''),
+        temp = [];
+
+    res.reverse();
+    res = res.join('');
+
+    for (var i = 0; i < res.length; i+=3) {
+         temp.push( res.substr(i, 3) );
+     }
+
+     temp = temp.join(' ');
+     temp = temp.split('');
+     temp = temp.reverse();
+     temp = temp.join('');
+
+    return temp;
 }
