d55c3a40b0ef66d0685d5c5f082367acbd367096
dimadiam <43180314+dimadiam@users.noreply.github.com>
2019-06-17 15:20:31 +0300

    Update virus.js

--- virus.js
+++ virus.js
@@ -1,11 +1,11 @@
 class Virus {
     constructor (health, chance, minTemp, maxTemp, country) {
-    this.health = health; // количество здоровья (если достигнет 0, то вирус умер)
-    this.chance = chance; // шанс заражения человека в %
-    this.minTemp = minTemp; // если ниже - умер
-    this.maxTemp = maxTemp; // если выше - умер
-    this.country = country; // в какой стране находится
-    this.alive = true; // вирус жив или нет
+    this.health = health; 
+    this.chance = chance; 
+    this.minTemp = minTemp;
+    this.maxTemp = maxTemp;
+    this.country = country;
+    this.alive = true; 
     this.gens = ['health', 'chance', 'minTemp', 'maxTemp'];
 }
     calcChance() {
@@ -14,7 +14,7 @@ class Virus {
     };
     mutation() {
         let gen = rnd(0, this.gens.length - 1);
-        gen = this.gens[gen]; // name of property
+        gen = this.gens[gen]; 
 
         let bin = this[gen].toString(2); // в двоичной системе число -> свойство вируса
         bin = invert(bin, rnd(0, bin.length)); // мутировавшее число
